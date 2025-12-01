46f71fc1e97f93195368ad29f08d0f4b886ac8ed
abbyxr <abbyxr14982@gmail.com>
2025-12-01 16:13:14 -0500

    testing

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "arithmetic.h"
+#include "geometry.h"
+#include "conversion.h"
 using namespace std;
 
 int main() {
@@ -8,5 +10,16 @@ int main() {
     cout << sub(3,2) << endl;
     cout << multiply(5,2) << endl;
     cout << divide(10,2) << endl;
+    //geometry
+    geometry g;
+    cout << g.areaOfCircle(9) << endl;
+    cout << g.areaOfRectangle(5,6) << endl;
+    cout << g.volumeOfBox(3,6,8) << endl;
+    //conversion
+    cout << celsiusToFahrenheit(23.3) << endl;
+    cout<< fahrenheitToCelsius(75.8) << endl;
+    cout << inchesToCentimeters(12)<< endl;
+    cout << centimetersToInches(63)<< endl;
+
     return 0;
 }
